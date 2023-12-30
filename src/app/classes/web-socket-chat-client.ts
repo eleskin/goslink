3c3a4d943502863460353df4ffdfcce9a38db0e3
eleskin <eleskin1201@gmail.com>
2023-12-30 13:34:47 +0300

    Created websocket service

--- src/app/classes/web-socket-chat-client.ts
+++ src/app/classes/web-socket-chat-client.ts
@@ -1,4 +1,11 @@
-class WebSocketChatClient {
+enum WebSocketChatClientEventMessageType {
+  NEW_MESSAGE = 'NEW_MESSAGE',
+  GET_MESSAGES = 'GET_MESSAGES',
+  UPDATE_MESSAGE = 'UPDATE_MESSAGE',
+  DELETE_MESSAGES = 'DELETE_MESSAGES',
+}
+
+class WebSocketChatClient extends WebSocket {
 
 }
 
--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import WebSocketChatClient from '../../classes/web-socket-chat-client';
+import UserStore from '../../store/user/user.store';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
+  private userStore = inject(UserStore);
 
-  constructor() { }
+  constructor() {
+    const {_id} = this.userStore.user();
+    const webSocketUrl = `ws://localhost:8000/api/websocket/?_id=${_id}`;
+
+    const webSocket = new WebSocketChatClient(webSocketUrl);
+  }
 }
