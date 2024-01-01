ae8a083434f3712b0578c0ec16a575378bf661ea
eleskin <eleskin1201@gmail.com>
2024-01-01 05:16:34 +0300

    Refactoring

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component, inject} from '@angular/core';
 import {RoomsListComponent} from '../../components/rooms-list/rooms-list.component';
 import {ChatComponent} from '../../components/chat/chat.component';
 import {NgIf} from '@angular/common';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import WebsocketStore from '../../store/websocket/websocket.store';
 import {WebsocketService} from '../../services/websocket/websocket.service';
 import WebSocketChatClient from '../../classes/web-socket-chat-client';
@@ -23,7 +23,7 @@ export class HomeComponent {
   protected visibleChat: boolean = Boolean(this.roomId);
   private readonly webSocketStore = inject(WebsocketStore);
 
-  constructor(private route: ActivatedRoute, private websocketService: WebsocketService) {
+  constructor(private route: ActivatedRoute, private websocketService: WebsocketService, private router: Router) {
     console.log(this.roomId);
     this.websocketService.webSocket = new WebSocketChatClient('ws://localhost:8000/api/websocket');
 
@@ -33,5 +33,12 @@ export class HomeComponent {
     // this.webSocketStore.setContactId(this.contactId);
     // this.websocketService.webSocket?.addEventListener('open', () => {
     // });
+    this.router.events.subscribe(async (value) => {
+      if (value instanceof NavigationEnd) {
+        if (this.route.snapshot.paramMap.get('_id')) {
+          this.webSocketStore.setSearchedUser(null);
+        }
+      }
+    });
   }
 }
