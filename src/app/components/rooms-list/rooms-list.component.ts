790e4bc895b116ab6951822b4f0bac8658c55945
eleskin <eleskin1201@gmail.com>
2024-01-01 11:01:46 +0300

    Refactoring

--- src/app/components/rooms-list/rooms-list.component.ts
+++ src/app/components/rooms-list/rooms-list.component.ts
@@ -4,10 +4,6 @@ import truncate from '../../utils/truncate';
 import {InputComponent} from '../input/input.component';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import User from '../../interfaces/user';
-import RoomsStore from '../../store/rooms/rooms.store';
-import Room from '../../interfaces/room';
-import {UserService} from '../../services/user/user.service';
-import ChatStore from '../../store/chat/chat.store';
 import WebsocketStore from '../../store/websocket/websocket.store';
 import {WebsocketService} from '../../services/websocket/websocket.service';
 
@@ -27,24 +23,14 @@ import {WebsocketService} from '../../services/websocket/websocket.service';
 export class RoomsListComponent {
   protected readonly truncate = truncate;
   protected conversationalist: string = this.route.snapshot.paramMap.get('_id') ?? '';
-  // private readonly chatStore = inject(ChatStore);
   private readonly webSocketStore = inject(WebsocketStore);
   protected searchedUser: User | null = this.webSocketStore?.searchedUser();
   protected rooms: User[] = this.webSocketStore?.rooms();
 
   constructor(
     protected route: ActivatedRoute,
-    private userService: UserService,
     private websocketService: WebsocketService,
   ) {
-    // effect(() => {
-    //   this.rooms = this.webSocketStore.rooms();
-    //   this.rooms = this.webSocketStore.rooms().map((room: any) => {
-    //     room.online = true;
-    //     room.online = this.chatStore.onlineUsers().includes(room.conversationalist);
-    // return room;
-    // });
-    // });
     effect(() => {
       this.searchedUser = this.webSocketStore?.searchedUser();
       this.rooms = this.webSocketStore?.rooms();
@@ -60,13 +46,5 @@ export class RoomsListComponent {
         contactUsername: event.target.value.slice(1),
       },
     }));
-    // const user: User = await this.userService.searchUser(event.target.value);
-    //
-    // if (Object.keys(user).length) {
-    //   this.searchedUsers.length = 0;
-    //   this.searchedUsers.push(user);
-    // } else {
-    //   this.searchedUsers.length = 0;
-    // }
   }
 }
