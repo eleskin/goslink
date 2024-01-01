e90e41c2cb6c83416d5ba15f25fd0eefc9f7ec8a
eleskin <eleskin1201@gmail.com>
2024-01-01 16:24:19 +0300

    Refactoring

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -4,6 +4,7 @@ import Message from '../../interfaces/message';
 import UserStore from '../../store/user/user.store';
 import User from '../../interfaces/user';
 import {WebsocketService} from '../../services/websocket/websocket.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-message',
@@ -26,7 +27,7 @@ export class MessageComponent {
   private readonly userStore = inject(UserStore);
   private user: User = this.userStore.user();
 
-  constructor(private webSocketService: WebsocketService) {
+  constructor(private webSocketService: WebsocketService, private route: ActivatedRoute) {
     effect(() => {
       this.user = this.userStore.user();
     });
@@ -41,6 +42,8 @@ export class MessageComponent {
       type: 'DELETE_MESSAGE',
       data: {
         _id: message._id,
+        userId: this.user._id,
+        contactId: this.route.snapshot.paramMap.get('_id') ?? '',
       },
     }));
   }
