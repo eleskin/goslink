44ef52cf9ef416188b3b3c00bf2e2c2e8a620a2d
eleskin <eleskin1201@gmail.com>
2024-01-03 14:58:43 +0300

    Add checked indicate

--- src/app/components/chat-container/chat-container.component.ts
+++ src/app/components/chat-container/chat-container.component.ts
@@ -23,6 +23,7 @@ export class ChatContainerComponent {
   protected messages: Message[] = this.webSocketStore.messages();
   @ViewChild('chat') private chatRef: ElementRef<HTMLDivElement> | undefined;
   @Input() public setEdit!: (event: boolean, message?: Message) => void;
+  private observer: IntersectionObserver | undefined;
 
   constructor(private webSocketService: WebsocketService) {
     this.webSocketService.webSocket?.addEventListener('message', () => {
@@ -35,6 +36,48 @@ export class ChatContainerComponent {
 
     effect(() => {
       this.messages = this.webSocketStore.messages();
+      setTimeout(() => {
+        this.setupIntersectionObserver();
+      }, 0)
+    });
+  }
+
+  private markAsRead(messageId: string): void {
+    console.log(messageId);
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.setupIntersectionObserver();
+    }, 0)
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  private setupIntersectionObserver(): void {
+    const options = {
+      root: this.chatRef?.nativeElement,
+      rootMargin: '0px',
+      threshold: 1.0
+    };
+
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const messageElement = entry.target as HTMLElement;
+          messageElement.getAttribute('id') && this.markAsRead(messageElement.getAttribute('id') ?? '');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    this.messages.forEach((message) => {
+      const element = document.querySelector(`#message-${message._id}`);
+      element && this.observer?.observe(element);
     });
   }
 }
