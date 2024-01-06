cdd44aea05baf8b220ea08845d1771d64cac498f
eleskin <eleskin1201@gmail.com>
2024-01-06 17:57:12 +0300

    Refactoring

---
+++ src/app/components/rooms-item/rooms-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms-item.component.html',
+  styleUrl: './rooms-item.component.css'
+})
+export class RoomsItemComponent {
+
+}
