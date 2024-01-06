364d99eb7dcb840ceb0c5b6ff209ce008aa6d359
eleskin <eleskin1201@gmail.com>
2024-01-06 10:16:26 +0300

    Add new user modal

--- src/app/ui/modal/modal.component.ts
+++ src/app/ui/modal/modal.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+  ],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  protected visibleModal = true;
 
+  protected handleClickOverlay() {
+    this.visibleModal = false;
+  }
 }
