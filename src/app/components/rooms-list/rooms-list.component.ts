7a001e421e621da37267599b64ac6c236582c487
eleskin <eleskin1201@gmail.com>
2024-01-06 20:24:13 +0300

    Refactoring

--- src/app/components/rooms-list/rooms-list.component.ts
+++ src/app/components/rooms-list/rooms-list.component.ts
@@ -1,11 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {RoomsItemComponent} from '../rooms-item/rooms-item.component';
+import Room from '../../interfaces/room';
+import WebsocketStore from '../../store/websocket/websocket.store';
+import {ActivatedRoute, NavigationEnd, Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-rooms-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    RoomsItemComponent,
+    RouterLink,
+  ],
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.css',
 })
 export class RoomsListComponent {
+  private readonly webSocketStore = inject(WebsocketStore);
+  protected rooms: Room[] = [];
+  protected contactId: string = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.router.events.subscribe(async (value) => {
+      if (value instanceof NavigationEnd) {
+        this.contactId = this.route.snapshot.paramMap.get('_id') ?? '';
+      }
+    });
+
+    effect(() => {
+      this.rooms = this.webSocketStore.rooms();
+    });
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,43 +1,23 @@
-import {Component, effect, EventEmitter, inject, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {InputComponent} from '../../ui/input/input.component';
-import {NgForOf, NgOptimizedImage} from '@angular/common';
-import {RoomsItemComponent} from '../rooms-item/rooms-item.component';
-import {ActivatedRoute, NavigationEnd, Router, RouterLink} from '@angular/router';
-import Room from '../../interfaces/room';
-import WebsocketStore from '../../store/websocket/websocket.store';
+import {NgOptimizedImage} from '@angular/common';
+import {RoomsListComponent} from '../rooms-list/rooms-list.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     InputComponent,
-    NgForOf,
     NgOptimizedImage,
-    RoomsItemComponent,
-    RouterLink,
+    RoomsListComponent,
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
   @Output() public handleOpenNewChatModal = new EventEmitter<boolean>();
-  protected contactId: string = '';
-  protected rooms: Room[] = [];
-  private readonly webSocketStore = inject(WebsocketStore);
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {
-    this.router.events.subscribe(async (value) => {
-      if (value instanceof NavigationEnd) {
-        this.contactId = this.route.snapshot.paramMap.get('_id') ?? '';
-      }
-    });
-
-    effect(() => {
-      this.rooms = this.webSocketStore.rooms();
-    });
+  constructor() {
   }
 
   protected handleClickNewChatButton() {
