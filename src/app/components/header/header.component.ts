d8c3614a1f31759f500cb5de2e36fd1bd5b911d0
eleskin <eleskin1201@gmail.com>
2024-01-06 17:25:15 +0300

    Refactoring

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component, effect, inject} from '@angular/core';
 import {NgIf, NgOptimizedImage} from '@angular/common';
-import {NavigationEnd, Router, RouterLink} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {UserService} from '../../services/user/user.service';
 import UserStore from '../../store/user/user.store';
 import User from '../../interfaces/user';
@@ -10,21 +10,28 @@ import User from '../../interfaces/user';
   standalone: true,
   imports: [
     NgOptimizedImage,
-    RouterLink,
     NgIf,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  openPopup: boolean = false;
-  disabledBackButton: boolean = true;
+  protected openPopup: boolean = false;
+  protected disabledBackButton: boolean = true;
   private readonly userStore = inject(UserStore);
   protected user: User = this.userStore.user();
 
   constructor(private userService: UserService, private router: Router) {
     effect(() => {
       this.user = this.userStore.user();
+    });
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(async (value) => {
+      if (value instanceof NavigationEnd) {
+        this.disabledBackButton = value.url === '/';
+      }
     });
   }
 
@@ -40,13 +47,5 @@ export class HeaderComponent {
 
   protected async handleClickBack() {
     await this.router.navigate(['/']);
-  }
-
-  ngOnInit() {
-    this.router.events.subscribe(async (value) => {
-      if (value instanceof NavigationEnd) {
-        this.disabledBackButton = value.url === '/';
-      }
-    });
   }
 }
