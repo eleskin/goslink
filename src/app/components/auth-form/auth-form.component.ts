ce4ac40ccade80d3c64d4c95003b056db7bfb052
eleskin <eleskin1201@gmail.com>
2024-01-07 07:31:24 +0300

    Refactoring

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from '../../ui/button/button.component';
+import {CheckboxComponent} from '../../ui/checkbox/checkbox.component';
+import {FormComponent} from '../../ui/form/form.component';
+import {InputComponent} from '../../ui/input/input.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-  imports: [],
+	imports: [
+		ButtonComponent,
+		CheckboxComponent,
+		FormComponent,
+		InputComponent,
+		ReactiveFormsModule,
+		RouterLink,
+	],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.css'
 })
