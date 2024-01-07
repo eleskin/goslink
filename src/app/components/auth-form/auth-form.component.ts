64965aa667326b4bfe4eed311e935bcbae9eab17
eleskin <eleskin1201@gmail.com>
2024-01-07 07:38:04 +0300

    Refactoring

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -3,24 +3,32 @@ import {ButtonComponent} from '../../ui/button/button.component';
 import {CheckboxComponent} from '../../ui/checkbox/checkbox.component';
 import {FormComponent} from '../../ui/form/form.component';
 import {InputComponent} from '../../ui/input/input.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-	imports: [
-		ButtonComponent,
-		CheckboxComponent,
-		FormComponent,
-		InputComponent,
-		ReactiveFormsModule,
-		RouterLink,
-	],
+  imports: [
+    ButtonComponent,
+    CheckboxComponent,
+    FormComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    RouterLink,
+    FormsModule,
+    NgIf,
+  ],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.css'
 })
 export class AuthFormComponent {
   @Input() handleFormSubmit!: (event: any) => void;
   @Input() formType!: 'login' | 'register';
+  protected nameValue: string = '';
+  protected usernameValue: string = '';
+  protected emailValue: string = '';
+  protected passwordValue: string = '';
+  protected rememberValue: boolean = false;
 }
