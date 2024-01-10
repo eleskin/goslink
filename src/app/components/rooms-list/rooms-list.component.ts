0de336a1b5f072615a2a34e4378a1f3ae23d960a
eleskin <eleskin1201@gmail.com>
2024-01-10 16:38:08 +0300

    Add group chats

--- src/app/components/rooms-list/rooms-list.component.ts
+++ src/app/components/rooms-list/rooms-list.component.ts
@@ -1,8 +1,10 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {NgForOf} from '@angular/common';
 import {RoomsItemComponent} from '../rooms-item/rooms-item.component';
 import {ActivatedRoute, NavigationEnd, Router, RouterLink} from '@angular/router';
 import User from '../../interfaces/user';
+import {WebsocketService} from '../../services/websocket/websocket.service';
+import UserStore from '../../store/user/user.store';
 
 @Component({
   selector: 'app-rooms-list',
@@ -21,8 +23,9 @@ export class RoomsListComponent {
   @Output() public handleVisibleModal = new EventEmitter<boolean>();
   @Input() public visibleModal = false;
   @Input() public searchList = false;
+  private readonly userStore = inject(UserStore);
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router, private webSocketService: WebsocketService) {
     this.router.events.subscribe(async (value) => {
       if (value instanceof NavigationEnd) {
         this.contactId = this.route.snapshot.paramMap.get('_id') ?? '';
@@ -33,8 +36,12 @@ export class RoomsListComponent {
   protected async handleCloseModal(room: User) {
     this.handleVisibleModal.emit(false);
 
-    if (this.searchList) {
-      await this.router.navigate([`/chat/${room._id}`], {queryParams: {message: room.lastMessage?._id}});
-    }
+    // if (this.searchList) {
+    //   await this.router.navigate([`/chat/${room._id}`], {queryParams: {message: room.lastMessage?._id}});
+    // }
+    this.webSocketService.webSocket?.sendJSON('NEW_CHAT', {
+      userId: this.userStore.user()._id,
+      contactId: room._id,
+    });
   }
 }
