992d8ae670e0a730e16b5481a86f05a4a1295467
eleskin <eleskin1201@gmail.com>
2024-01-15 21:36:49 +0300

    Refactoring

--- src/app/components/chat-header/chat-header.component.ts
+++ src/app/components/chat-header/chat-header.component.ts
@@ -3,11 +3,14 @@ import User from '../../interfaces/user';
 import WebsocketStore from '../../store/websocket/websocket.store';
 import {ActivatedRoute} from '@angular/router';
 import getGradientFromChar from '../../utils/getGradientFromChar';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-chat-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+  ],
   templateUrl: './chat-header.component.html',
   styleUrl: './chat-header.component.css',
 })
@@ -16,11 +19,16 @@ export class ChatHeaderComponent {
   protected online: boolean = false;
   protected readonly getGradientFromChar = getGradientFromChar;
   private readonly webSocketStore = inject(WebsocketStore);
+  protected chat: any;
 
   constructor(private route: ActivatedRoute) {
     effect(() => {
       this.contact = this.webSocketStore.contact();
       this.online = this.webSocketStore.onlineUsers().includes(this.route.snapshot.paramMap.get('_id') ?? '');
+
+      this.chat = this.webSocketStore.chats().find((chat) => chat._id === this.route.snapshot.paramMap.get('_id') ?? '');
     });
   }
+
+  protected handleAddUser() {}
 }
