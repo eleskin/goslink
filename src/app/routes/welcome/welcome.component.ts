81dc7b44be7b3e041b43773ef8021aca908d89d1
eleskin <eleskin1201@gmail.com>
2024-02-23 17:41:05 +0300

    Added welcome page

--- src/app/routes/welcome/welcome.component.ts
+++ src/app/routes/welcome/welcome.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {ButtonComponent} from '../../ui/button/button.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ButtonComponent,
+    RouterLink,
+  ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
