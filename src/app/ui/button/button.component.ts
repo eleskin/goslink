27dbf9152987da0d53351661a7b18f17b403ef71
eleskin <eleskin1201@gmail.com>
2024-02-23 18:01:31 +0300

    Added welcome page

--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -1,13 +1,19 @@
 import {Component, Input} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    RouterLink,
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
 export class ButtonComponent {
   @Input() text: string = '';
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() routerLink = '';
 }
