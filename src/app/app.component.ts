c4f81e7bb2fc6ad83f4d3003ceed2c4310477d39
eleskin <eleskin1201@gmail.com>
2024-02-25 08:54:41 +0300

    Fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
@@ -14,16 +14,4 @@ import 'normalize.css';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  ngOnInit() {
-    this.adjustHeight();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.adjustHeight();
-  }
-
-  adjustHeight() {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-  }
 }
