2be7a117395220685f3ba74bf837ebf163f3215c
eleskin <eleskin1201@gmail.com>
2024-02-25 09:19:58 +0300

    Fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import {RouterOutlet} from '@angular/router';
 import {MainComponent} from './components/main/main.component';
 
 import 'normalize.css';
-import {FullHeightDirective} from './full-height.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgOptimizedImage, MainComponent, FullHeightDirective],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
