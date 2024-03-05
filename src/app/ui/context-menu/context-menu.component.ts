ce7ff97259d4f8b750aea165244ab881a0412a83
eleskin <eleskin1201@gmail.com>
2024-03-06 00:01:17 +0300

    Added context-menu.component

---
+++ src/app/ui/context-menu/context-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-context-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './context-menu.component.html',
+  styleUrl: './context-menu.component.css'
+})
+export class ContextMenuComponent {
+
+}
